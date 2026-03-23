00b6310193c7a3d2e66199040b65fed156c411f7
shaybeauty <shayannegof@gmail.com>
2026-03-23 08:52:06 +0000

    V48😊🟣🚿

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shay-finance-v45';
+const CACHE_NAME = 'shay-finance-v46';
 const CDN_ASSETS = [
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
